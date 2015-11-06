a0052091049dee1e475991fc4a0eb8c9bd0f0f9a
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2015-11-05 21:01:19 -0500

    added healthcheck endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var server = require('nodebootstrap-server');
+var healthcheck = require('connect-service-healthcheck');
 
 server.setup(function(runningApp) {
 
@@ -6,5 +7,19 @@ server.setup(function(runningApp) {
   runningApp.engine('handlebars', require('hbs').__express);
 
   runningApp.use('/shipments/:tracking_number', require('shipment_detail'));
+
+  // Healthcheck
+  var pjson = require('./package.json');
+  runningApp.use( '/healthcheck',
+    healthcheck({
+      componentHealthchecks: function() {
+        return {foo: BPromise.resolve('foo is good')};
+      },
+      memoryName: 'VerySecretUsername',
+      memoryPass: 'MuchMoreSecretPassword',
+      version: pjson.version
+    })
+  );
+
 
 });
